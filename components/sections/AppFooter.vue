8405826f75109eb89d966f27fc1b99e3fe2a8a2c
rama11996 <ramanathanpri@gmail.com>
2018-11-21 23:05:33 +0530

    Add Footer Component

--- components/sections/AppFooter.vue
+++ components/sections/AppFooter.vue
@@ -12,6 +12,27 @@ section.app-footer
       .section-form
         input(type="text" placeholder="Your Email")
         button.btn Subscribe
+    .social-media
+      ul
+        li
+          img(src="/images/facebook.png")
+        li
+          img(src="/images/twitter.png")
+        li
+          img(src="/images/google+.png")
+        li
+          img(src="/images/linkedin.png")    
+        li
+          img(src="/images/instagram.png")
+    .footer-menu
+      ul
+        li About Event
+        li Blogs
+        li Contact
+        li Tickets
+        li Venue   
+    .copyrights
+      p Copyright © 2018 Exhibz. All Rights Reserved.     
 </template>
 
 <script>
@@ -26,7 +47,6 @@ section.app-footer
   background-image: url(/images/book-seat-img.jpg)
   background-repeat: no-repeat
   background-size: cover
-  
   min-height: 50vh
 section.app-footer
   .container
@@ -39,35 +59,51 @@ section.app-footer
         color: $white
         padding: $space*2
       .btn
+        padding: 0 $space*2
+        line-height: $space*3.2
 
+section.app-footer
+  .container
+    .newsletter
+      background-image: url(/images/subcribe-pattern.png)
+      background-color: green
+      margin: 0 auto
+      padding: $space*2
+      margin: $space
+      .section-title
+        padding-top: $space*2
+        span
+          padding: $space*2
+          font-size: $space
+          color: $white
+          font-weight: 800
+        h2
+          padding: $space*2 0
+          font-weight: 800
+          color: $white
+        
+      .section-form   
+        @include flex 
+        align-items: flex-end
+        input
+          width: 100%
+          border: none
+          border-bottom: 1px solid #6f55b0
+          background: none 
+          padding: 1rem     
+        .btn
+          padding: 0 $space*2
+          line-height: $space*3.2
 section
-  .newsletter
-    background-image: url(/images/subcribe-pattern.png)
-    width: 70rem
-    height: 25rem
-    // background-color: green
-    margin: 0 auto
-    padding-top: $space*3
-    .section-title
-      padding-top: $space*3
-      letter-spacing: $space/2
-      span
-        padding: $space*2
-        font-size: $space
-        color: $white
-        font-weight: 800
-      h2
-        padding: $space*2 0
-        font-weight: 800
-        color: $white
-    .section-form    
-      input
-        border: none
-        border-bottom: 2px solid 
-        background: none 
-        padding: 1rem     
-
-  
+  .container
+    .social-media, .footer-menu
+      text-align: center
+      ul
+        li
+          display: inline-block
+      img
+        width: 25px
+    
        
     
 
