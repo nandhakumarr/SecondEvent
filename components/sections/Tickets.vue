ceeab33da494f8da62eb60cbde55283742caf34f
AP Keerthika <keerthi2592@gmail.com>
2018-11-22 13:18:52 +0530

    Add ticket section

--- components/sections/Tickets.vue
+++ components/sections/Tickets.vue
@@ -5,11 +5,31 @@ section.tickets
       span Pricing Plans
       h2 Get your Ticket
     main
+      .pricing-item(v-for="(t, i) in data" key="i")
+        img.dot(src="/images/dot.png")
+        .pricing-box
+          .pricing-header
+            h2 {{ t.title }}
+            h3 {{ t.price }}
+            p Available tickets for this price
+          .pricing-progress
+            .progress-inner
+            p {{ t.progress }}
+          .promotional-code
+            p.text Enter Promotional code
+            a.btn {{ t.cta }}
+            p All prices exclude 25% VAT
+        img.dot1(src="/images/dot.png")
 </template>
 
 <script>
+import ticket from 'static/seed/tickets'
 export default {
-
+  data () {
+    return {
+      data: ticket
+    }
+  }
 }
 </script>
 
@@ -19,7 +39,7 @@ section.tickets
   background-image: url(/images/pricing-img.jpg)
   background-repeat: no-repeat
   background-size: cover
-  min-height: 100vh
+  min-height: 120vh
   position: relative
   &:before
     @include absolute
@@ -29,7 +49,84 @@ section.tickets
     height: 100%
     background-image: linear-gradient(144deg, #321575 0%, #8d0b93 51%, #ff0066 100%, #ff057c 100%)
   .container
+    main
+      @include flex
+      flex-wrap: wrap
+      position: relative
+      .pricing-item
+        position: relative
+        text-align: center
+        .pricing-box
+          background: $white
+          padding: $space*2.5
 
 
+.dot
+  display: block
+  width: 100%
+.dot1
+  transform: rotate(180deg)
+  display: block
+  width: 100%
+  margin-top: -2px
+
+.pricing-header
+  h2, h3
+    color: $event-textblue
+  p
+    color: $neutral-light
+    margin-bottom: $space
+  h2
+    font-size: 1.2rem
+    font-weight: 800
+    line-height: 1.5rem
+    margin-bottom: $space
+  h3
+    font-size: 3.5rem
+    letter-spacing: 1px
+    margin-bottom: $space*1.5
+
+
+.pricing-progress
+  width: 100%
+  height: 10px
+  background: #ebedf4
+  margin-bottom: $space*1.5
+  .progress-inner
+    margin-bottom: $space
+    background-image: linear-gradient(144deg, #321575 0%, #8d0b93 51%, #ff0066 100%, #ff057c 100%)
+    height: 100%
+  p
+    color: $event-pink
+    font-weight: 800
+    margin-bottom: $space*2
+
+
+.promotional-code
+  position: relative
+  p.text
+    color: $event-pink
+    margin-bottom: $space*2
+    margin-top: $space*3
+  a.btn
+    &:before
+      @include absolute-w
+      content: ''
+      width: 25px
+      height: 25px
+      display: block
+      background: #fff
+      border-radius: 50%
+    &:after
+      @include absolute-e
+      content: ''
+      width: 25px
+      height: 25px
+      display: block
+      background: #fff
+      border-radius: 50%
+  p
+    color: $neutral-light
+    margin-top: $space*2
 </style>
 
---
+++ static/seed/tickets.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'EARLY BIRD',
+    price: '$219',
+    progress: '500/500',
+    cta: 'Buy Ticket'
+  },
+  {
+    title: 'REGULAR',
+    price: '$399',
+    progress: '350/500',
+    cta: 'Buy Ticket'
+  },
+  {
+    title: 'PLATINUM',
+    price: '$399',
+    progress: '250/500',
+    cta: 'Buy Ticket'
+  },
+]
