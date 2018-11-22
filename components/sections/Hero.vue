5bd426cdde3502b7472287fb82ab3246f503e62d
AP Keerthika <keerthi2592@gmail.com>
2018-11-22 05:49:31 +0530

    Add hero with responsive

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -2,7 +2,14 @@
 section.hero
   .container
     app-header
-
+    .banner-content
+      p 5 to 7 June 2019, Waterfront Hotel, London
+      h1 Digital Thinkers MeetUp
+      .timer
+      .cta
+        a.btn Buy Ticket
+    .banner-img
+      img(src="/images/banner-img.png")
 </template>
 
 <script>
@@ -22,6 +29,26 @@ section.hero
   background-repeat: no-repeat
   background-size: cover
   background-position: center center
-  min-height: 100vh
+  // min-height: 75vh
+  color: $white
+  padding-top: $space*12
+  .container
+    @include spread
+    .banner-content
+      h1
+        text-transform: uppercase
+        margin-bottom: $space*3.5
+        margin-top: $space*2
+        line-height: 1.2
+        @media (max-width: $breakpoint-tab-1)
+          font-size: 1.8rem
+    .banner-img
+      position: relative
+      min-height: 1px
+      img
+        vertical-align: middle
+      //   @include absolute-e
+      @media (max-width: $breakpoint-tab-1)
+        display: none
 </style>
 
