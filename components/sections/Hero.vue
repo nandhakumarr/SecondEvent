6addd47f877e177c598f16437330a26ae80e138e
AP Keerthika <keerthi2592@gmail.com>
2018-11-22 06:48:19 +0530

    Fix css

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -28,26 +28,28 @@ section.hero
   background-image: url(/images/banner-bg.jpg)
   background-repeat: no-repeat
   background-size: cover
-  background-position: center center
-  // min-height: 75vh
   color: $white
   padding-top: $space*7
   .container
     @include spread
     .banner-content
+      margin-bottom: $space*15
+      @media (max-width: $breakpoint-mobile)
+        margin-bottom: $space*5
       h1
         text-transform: uppercase
         margin-bottom: $space*3.5
         margin-top: $space*2
         line-height: 1.2
         @media (max-width: $breakpoint-tab-1)
+          font-size: 2rem
+        @media (max-width: $breakpoint-mobile)
           font-size: 1.8rem
     .banner-img
       position: relative
-      min-height: 1px
       img
+        max-width: 100%
         vertical-align: middle
-      //   @include absolute-e
       @media (max-width: $breakpoint-tab-1)
         display: none
 </style>
