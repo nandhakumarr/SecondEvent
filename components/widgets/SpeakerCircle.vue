29a34b41af8fcc2f218a7318d3ea97bc683d3eb0
manojjonam <manoj.jonam.boxer@gmail.com>
2018-11-30 12:30:52 +0530

    Abstract speaker(circle) and speaker modal as widgets

---
+++ components/widgets/SpeakerCircle.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+.speaker
+  .speaker-img
+    img(:src="speaker.image")
+    .plus-image
+      img(src="https://img.icons8.com/ios/48/cccccc/plus.png")
+  .speaker-info
+    h3 {{ speaker.name }}
+    p {{ speaker.designation }}
+</template>
+
+<script>
+export default {
+  props: {
+    speaker: {
+      type: Object,
+      default: {}
+    }
+  }
+}
+</script>
+
+
+<style lang="sass" scoped>
+@import 'assets/styles/includes'
+
+.speaker
+  margin: $space
+  position: relative
+  &:hover
+    .speaker-info
+      color: $event-pink !important
+    .speaker-img
+      &:before
+        position: absolute
+        left: 0
+        top: 0
+        width: 100%
+        height: 100%
+        content: ''
+        background: rgba(59, 29, 130, 0.5)
+        transition: all 0.4s ease
+        opacity: 1
+        z-index: 1
+        cursor: pointer
+      img
+        transform: scale(1.2)
+        transition: all 0.6s ease
+      .plus-image
+        opacity: 1
+        position: absolute
+        top: 50%
+        left: 37%
+        z-index: 10
+        color: #fff
+        font-size: 22px
+        width: 50px
+        height: 50px
+        margin: auto
+        // padding: 12px 0
+        // border: 1spx solid #ddd
+        transform: translateY(-50%)
+        border-radius: 50%
+        cursor: pointer
+  .speaker-img
+    border-radius: 50%
+    width: 15rem
+    height: 15rem
+    position: relative
+    margin: auto auto 1.2rem
+    overflow: hidden
+    img
+      width: 100%
+    .plus-image
+      opacity: 0
+      img
+        color: $white
+  .speaker-info
+    p
+      color: $neutral-light
+    h3
+      &:hover
+        color: $event-pink
+        cursor: pointer
+.speaker
+    .speaker-img
+      @media (max-width: $breakpoint-minitab)
+        margin: auto 6rem 1.2rem !important
+</style>
---
+++ components/widgets/SpeakerModal.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+.speaker-modal
+  .modal-content
+    .mspeaker-img
+      img(:src="speaker.image")
+    .mspeaker-info
+      h3 {{ speaker.name }}
+      p {{ speaker.designation }}
+      img(:src="speaker.logo")
+      p {{ speaker.content }}
+      h4 Session by {{ speaker.name }}
+      .sessions
+        .day1
+          h4 Day1
+          span {{ speaker.time }}
+          p {{ speaker.topic }}
+        .day2
+          h4 Day2
+          span {{ speaker.time }}
+          p {{ speaker.topic }}
+    button
+      img(src="https://img.icons8.com/metro/48/000000/multiply.png" @click="close()")
+</template>
+
+<script>
+export default {
+  props: {
+    speaker: {
+      type: Object,
+      default: {}
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import 'assets/styles/includes'
+
+.speaker-modal
+  @include fixed
+  @include flex
+  background: #f4f4f4
+  z-index: 1000
+  max-width: $page-width
+  margin: auto
+  align-items: flex-start
+  height: 35rem
+  overflow: auto
+  .modal-content
+    @include flex
+    align-items: flex-start
+    text-align: left
+    @media (max-width: $breakpoint-tab-3)
+      display: block
+    .mspeaker-info
+      padding: $space*3
+    p
+      color: $neutral-light
+    h4, span
+      color: $black
+    p, h4, h3,
+      padding-bottom: $space
+    .sessions
+      @include flex
+      justify-content: flex-start
+      .day1, .day2
+        h4
+          padding: 0rem
+      p
+        color: #e7015e
+      span
+        color: $neutral-light
+        font-weight: bold
+      .day2
+        margin-left: $space*3
+
+  button
+    border: none
+    padding: $space
+    cursor: pointer
+    background: none
+    img
+      width: $space*1.5
+</style>
+
